75a560ef769d9c337098e9d6a6022231ae76190a
yuan442177 <15973370670@163.com>
2019-04-02 11:52:21 +0800

    模板优化和链接优化

---
+++ resources/views/welcome/index/about.blade.php
@@ -0,0 +1,2 @@
+@extends('_layout.default')
+@section('title', '关于我们')
--- route/route.php
+++ route/route.php
@@ -12,3 +12,4 @@
 Route::get('', 'welcome/index/home'); 
 Route::get('/help', 'welcome/index/help'); 
 Route::get('/about', 'welcome/index/about');
+Route::get('/signup', 'user/auth/create');
